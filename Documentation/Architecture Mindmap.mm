211435bb5a16ce3d25667ad445e9bf2a07d6286a
Sparky0904 <sparky0904@gmail.com>
2019-05-19 15:45:26 +0100

    update

--- Documentation/Architecture Mindmap.mm
+++ Documentation/Architecture Mindmap.mm
@@ -1,9 +1,28 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1558274628974" ID="ID_183492621" MODIFIED="1558274695906" TEXT="Gym Tracker">
-<node CREATED="1558274696311" ID="ID_105710367" MODIFIED="1558274701185" POSITION="right" TEXT="Features">
-<node CREATED="1558274701598" ID="ID_1143257291" MODIFIED="1558274713594" TEXT="Create your own training sessions"/>
-<node CREATED="1558274715542" ID="ID_254520612" MODIFIED="1558274733474" TEXT="Track statistics times, weights lifted"/>
+<node CREATED="1558274628974" ID="ID_183492621" MODIFIED="1558274848977" TEXT="Gym Tracker">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1558274696311" ID="ID_105710367" MODIFIED="1558274848976" POSITION="right" TEXT="Features">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1558274701598" ID="ID_1143257291" MODIFIED="1558274848976" TEXT="Create your own training sessions">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1558274715542" ID="ID_254520612" MODIFIED="1558274848975" TEXT="Track statistics times, weights lifted">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1558274799239" ID="ID_1968558441" MODIFIED="1558274848975" POSITION="left" TEXT="Data">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1558274802183" ID="ID_1828804729" MODIFIED="1558274848974" TEXT="Schema">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1558274811063" ID="ID_270749782" MODIFIED="1558274848973" TEXT="UserDetails">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1558274821646" ID="ID_429834050" MODIFIED="1558274848973" TEXT="Training Shedule">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1558275846240" ID="ID_532610156" MODIFIED="1558275854742" TEXT="Excercise"/>
+</node>
 </node>
 </node>
 </map>
