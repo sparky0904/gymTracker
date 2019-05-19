02ccf4a465fb7c53b2d8aa7efab697ad82feee05
Sparky0904 <sparky0904@gmail.com>
2019-05-19 15:06:10 +0100

    Create Architecture Mindmap.mm

---
+++ Documentation/Architecture Mindmap.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1558274628974" ID="ID_183492621" MODIFIED="1558274695906" TEXT="Gym Tracker">
+<node CREATED="1558274696311" ID="ID_105710367" MODIFIED="1558274701185" POSITION="right" TEXT="Features">
+<node CREATED="1558274701598" ID="ID_1143257291" MODIFIED="1558274713594" TEXT="Create your own training sessions"/>
+<node CREATED="1558274715542" ID="ID_254520612" MODIFIED="1558274733474" TEXT="Track statistics times, weights lifted"/>
+</node>
+</node>
+</map>
